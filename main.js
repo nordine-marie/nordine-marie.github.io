191fb6fe0029cf693181684d5b4768d8dbf09142
Nordine MARIE <nordine.marie@telecom-sudparis.eu>
2021-01-26 01:20:38 +0100

    ADD PARTICLES BG

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-var language_switch = function() {
 
-}
+
+// PARTICLES-JS
+
+particlesJS.load('particles-js', './jslibs/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
